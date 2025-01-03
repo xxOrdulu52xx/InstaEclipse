4bd66ef129f0a7ad7ffd77efc28ddcda3af2435c
Abdul <abdulhaleemtamimi@gmail.com>
2025-01-04 00:07:26 +0300

    Enhance DevOptions detection

--- app/src/main/java/ps/reso/instaeclipse/mods/DevOptionsEnable.java
+++ app/src/main/java/ps/reso/instaeclipse/mods/DevOptionsEnable.java
@@ -2,6 +2,7 @@ package ps.reso.instaeclipse.mods;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
@@ -18,6 +19,7 @@ public class DevOptionsEnable {
             XposedBridge.log("(DevOptionsEnable) Error handling Dev Options: " + e.getMessage());
         }
     }
+
     private void handleAutoMode(XC_LoadPackage.LoadPackageParam lpparam) {
         String characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -32,33 +34,27 @@ public class DevOptionsEnable {
                         try {
                             Class<?> targetClass = XposedHelpers.findClass(classToHook, lpparam.classLoader);
                             Method[] methods = targetClass.getDeclaredMethods();
+                            Field[] fields = targetClass.getDeclaredFields();
 
-                                try {
-                                    for (Method method : targetClass.getDeclaredMethods()) {
-                                        if (methods.length == 1 && methods[0].getName().equals("A00") &&
-                                                methods[0].getReturnType() == Boolean.TYPE &&
-                                                methods[0].getParameterCount() == 1  && method.getParameterTypes()[0].getName().contains("UserSession")) {
+                            try {
+                                for (Method method : targetClass.getDeclaredMethods()) {
+                                    if (methods.length == 1 && methods[0].getName().equals("A00") &&
+                                            methods[0].getReturnType() == Boolean.TYPE &&
+                                            methods[0].getParameterCount() == 1 && method.getParameterTypes()[0].getName().contains("UserSession") &&
+                                            fields.length == 0 && Modifier.isFinal(method.getModifiers())) {
 
-                                            Class<?> declaringClass = methods[0].getDeclaringClass();
+                                        num_of_hooks += 1;
+                                        Class<?> UserSessionClass = XposedHelpers.findClass(Utils.USER_SESSION_CLASS, lpparam.classLoader);
+                                        hookDevOptions(targetClass, "A00", UserSessionClass);
 
-                                            if (declaringClass != null) {
-                                                Field[] fields = declaringClass.getDeclaredFields();
-                                                if (fields.length == 0) {
 
-                                                    num_of_hooks += 1;
-                                                    Class<?> UserSessionClass = XposedHelpers.findClass(Utils.USER_SESSION_CLASS, lpparam.classLoader);
-                                                    hookDevOptions(targetClass, "A00", UserSessionClass);
-
-                                                }
-
-                                            }
-                                        }
                                     }
-                                } catch (NoClassDefFoundError | XposedHelpers.ClassNotFoundError e) {
-                                    //XposedBridge.log("(DevOptionsEnable) Skipping method due to missing dependency: " + e.getMessage());
-                                } catch (Exception e) {
-                                    //XposedBridge.log("(DevOptionsEnable) General exception while inspecting method: " + e.getMessage());
                                 }
+                            } catch (NoClassDefFoundError | XposedHelpers.ClassNotFoundError e) {
+                                //XposedBridge.log("(DevOptionsEnable) Skipping method due to missing dependency: " + e.getMessage());
+                            } catch (Exception e) {
+                                //XposedBridge.log("(DevOptionsEnable) General exception while inspecting method: " + e.getMessage());
+                            }
 
                         } catch (NoClassDefFoundError | XposedHelpers.ClassNotFoundError e) {
                             //XposedBridge.log("(DevOptionsEnable) Skipping class " + classToHook + " due to missing dependency: " + e.getMessage());
@@ -69,7 +65,7 @@ public class DevOptionsEnable {
                 }
             }
 
-            if (num_of_hooks <= 0){
+            if (num_of_hooks <= 0) {
                 // No suitable classes found
                 XposedBridge.log("(DevOptionsEnable) No suitable classes found during dynamic search.");
             }
@@ -102,7 +98,7 @@ public class DevOptionsEnable {
         } catch (XposedHelpers.ClassNotFoundError e) {
             //XposedBridge.log("(DevOptionsEnable) XposedHelpers couldn't find class: " + targetClass.getName() + " or parameter class: " + secondTargetClass.getName() + " - " + e.getMessage());
         } catch (Exception e) {
-            XposedBridge.log("(DevOptionsEnable) General exception while hooking method: " + methodToHook + " in class: " + targetClass.getName() + " - " + e.getMessage());
+            //XposedBridge.log("(DevOptionsEnable) General exception while hooking method: " + methodToHook + " in class: " + targetClass.getName() + " - " + e.getMessage());
         }
     }
 }

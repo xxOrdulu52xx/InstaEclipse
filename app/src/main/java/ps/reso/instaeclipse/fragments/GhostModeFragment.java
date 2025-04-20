9ccbdae81364166672c243df661e304267442cd6
Abdul <abdulhaleemtamimi@gmail.com>
2025-04-20 09:53:30 +0300

    Updated ghost mode features

--- app/src/main/java/ps/reso/instaeclipse/fragments/GhostModeFragment.java
+++ app/src/main/java/ps/reso/instaeclipse/fragments/GhostModeFragment.java
@@ -17,7 +17,10 @@ import ps.reso.instaeclipse.R;
 
 public class GhostModeFragment extends Fragment {
     private static final String ENABLE_ALL_KEY = "enableAllGhostMode";
-    private static final String DM_KEY = "ghostModeDM";
+    private static final String DM_KEY = "ghostModeSeen";
+    private static final String TYPING_KEY = "ghostModeTyping";
+    private static final String SCREEN_SHOT_KEY = "ghostModeScreenShot";
+    private static final String VIEW_ONCE_KEY = "ghostModeViewOnce";
     private static final String STORY_KEY = "ghostModeStory";
     private static final String LIVE_KEY = "ghostModeLive";
 
@@ -25,6 +28,15 @@ public class GhostModeFragment extends Fragment {
     private Switch enableAllToggle;
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     private Switch dmToggle;
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch typingToggle;
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch screenShotToggle;
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch viewOnceToggle;
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     private Switch storyToggle;
     @SuppressLint("UseSwitchCompatOrMaterialCode")
@@ -33,6 +45,7 @@ public class GhostModeFragment extends Fragment {
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
 
+    @SuppressLint("MissingInflatedId")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -44,6 +57,9 @@ public class GhostModeFragment extends Fragment {
         // Initialize toggles
         enableAllToggle = view.findViewById(R.id.toggle_all);
         dmToggle = view.findViewById(R.id.dm_toggle);
+        typingToggle = view.findViewById(R.id.dm_typing_toggle);
+        screenShotToggle = view.findViewById(R.id.screenshot_toggle);
+        viewOnceToggle = view.findViewById(R.id.view_once_toggle);
         storyToggle = view.findViewById(R.id.story_toggle);
         liveToggle = view.findViewById(R.id.live_toggle);
 
@@ -54,12 +70,18 @@ public class GhostModeFragment extends Fragment {
         enableAllToggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
             saveToggleState(ENABLE_ALL_KEY, isChecked);
             dmToggle.setChecked(isChecked);
+            typingToggle.setChecked(isChecked);
+            screenShotToggle.setChecked(isChecked);
+            viewOnceToggle.setChecked(isChecked);
             storyToggle.setChecked(isChecked);
             liveToggle.setChecked(isChecked);
         });
 
         // Individual toggles
         dmToggle.setOnCheckedChangeListener((buttonView, isChecked) -> saveToggleState(DM_KEY, isChecked));
+        typingToggle.setOnCheckedChangeListener((buttonView, isChecked) -> saveToggleState(TYPING_KEY, isChecked));
+        screenShotToggle.setOnCheckedChangeListener((buttonView, isChecked) -> saveToggleState(SCREEN_SHOT_KEY, isChecked));
+        viewOnceToggle.setOnCheckedChangeListener((buttonView, isChecked) -> saveToggleState(VIEW_ONCE_KEY, isChecked));
         storyToggle.setOnCheckedChangeListener((buttonView, isChecked) -> saveToggleState(STORY_KEY, isChecked));
         liveToggle.setOnCheckedChangeListener((buttonView, isChecked) -> saveToggleState(LIVE_KEY, isChecked));
 
@@ -69,6 +91,9 @@ public class GhostModeFragment extends Fragment {
     private void loadToggleStates() {
         enableAllToggle.setChecked(preferences.getBoolean(ENABLE_ALL_KEY, false));
         dmToggle.setChecked(preferences.getBoolean(DM_KEY, false));
+        typingToggle.setChecked(preferences.getBoolean(TYPING_KEY, false));
+        screenShotToggle.setChecked(preferences.getBoolean(SCREEN_SHOT_KEY, false));
+        viewOnceToggle.setChecked(preferences.getBoolean(VIEW_ONCE_KEY, false));
         storyToggle.setChecked(preferences.getBoolean(STORY_KEY, false));
         liveToggle.setChecked(preferences.getBoolean(LIVE_KEY, false));
     }

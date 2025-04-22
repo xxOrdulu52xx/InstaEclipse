afe6af5f66ab8e706fc35c64260635029dc8591c
Abdul <abdulhaleemtamimi@gmail.com>
2025-04-22 11:03:12 +0300

    Version 0.3.2

--- app/src/main/java/ps/reso/instaeclipse/Xposed/Module.java
+++ app/src/main/java/ps/reso/instaeclipse/Xposed/Module.java
@@ -3,11 +3,7 @@ package ps.reso.instaeclipse.Xposed;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 import android.annotation.SuppressLint;
-import android.app.AndroidAppHelper;
 import android.os.Build;
-import android.os.Handler;
-import android.os.Looper;
-import android.widget.Toast;
 
 import org.luckypray.dexkit.DexKitBridge;
 
@@ -22,10 +18,19 @@ import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import ps.reso.instaeclipse.mods.DevOptionsEnable;
-import ps.reso.instaeclipse.mods.GhostModeDM;
-import ps.reso.instaeclipse.mods.Interceptor;
+import ps.reso.instaeclipse.mods.ads.AdBlocker;
+import ps.reso.instaeclipse.mods.ghostMode.LiveSeen;
+import ps.reso.instaeclipse.mods.ghostMode.ScreenshotDetection;
+import ps.reso.instaeclipse.mods.ghostMode.SeenState;
+import ps.reso.instaeclipse.mods.ghostMode.StorySeen;
+import ps.reso.instaeclipse.mods.ghostMode.TypingStatus;
+import ps.reso.instaeclipse.mods.ghostMode.ViewOnce;
 import ps.reso.instaeclipse.mods.misc.AutoPlayDisable;
 import ps.reso.instaeclipse.mods.misc.StoryFlipping;
+import ps.reso.instaeclipse.mods.misc.Testing;
+import ps.reso.instaeclipse.mods.network.Interceptor;
+import ps.reso.instaeclipse.utils.CustomToast;
+import ps.reso.instaeclipse.utils.FeatureStatusTracker;
 
 
 @SuppressLint("UnsafeDynamicallyLoadedCode")
@@ -33,12 +38,17 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
     private static final String IG_PACKAGE_NAME = "com.instagram.android";
     private static final String MY_PACKAGE_NAME = "ps.reso.instaeclipse";
     public static DexKitBridge dexKitBridge;
+    public static Boolean isShow_Follower_Status_Enabled;
+    public static ClassLoader hostClassLoader;
     private static String moduleSourceDir;
     private static String moduleLibDir;
     List<Predicate<URI>> uriConditions = new ArrayList<>();
     Boolean isDevEnabled;
     Boolean isGhost_Enabled;
-    Boolean isGhost_DM_Enabled;
+    Boolean isGhost_Seen_Enabled;
+    Boolean isGhost_Typing_Enabled;
+    Boolean isGhost_Screenshot_Enabled;
+    Boolean isGhost_View_Once_Enabled;
     Boolean isGhost_Story_Enabled;
     Boolean isGhost_Live_Enabled;
     Boolean isDistraction_Free_Enabled;
@@ -52,12 +62,15 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
     Boolean is_Misc_Enabled;
     Boolean isStop_Story_Flipping_Enabled;
     Boolean isStop_Video_AutoPlay_Enabled;
-    public static ClassLoader hostClassLoader;
+    Boolean isShow_Follower_Toast_Enabled;
+    Boolean isShow_Enabled_Features_Enabled;
 
     // for dev usage
+    /*
     public static void showToast(final String text) {
         new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(AndroidAppHelper.currentApplication().getApplicationContext(), text, Toast.LENGTH_LONG).show());
     }
+    */
 
     @Override
     public void initZygote(StartupParam startupParam) {
@@ -85,7 +98,10 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
             isGhost_Enabled = XposedPreferences.getPrefs().getBoolean("enableGhostMode", false);
 
             if (isGhost_Enabled) {
-                isGhost_DM_Enabled = XposedPreferences.getPrefs().getBoolean("ghostModeDM", false);
+                isGhost_Seen_Enabled = XposedPreferences.getPrefs().getBoolean("ghostModeSeen", false);
+                isGhost_Typing_Enabled = XposedPreferences.getPrefs().getBoolean("ghostModeTyping", false);
+                isGhost_Screenshot_Enabled = XposedPreferences.getPrefs().getBoolean("ghostModeScreenShot", false);
+                isGhost_View_Once_Enabled = XposedPreferences.getPrefs().getBoolean("ghostModeViewOnce", false);
                 isGhost_Story_Enabled = XposedPreferences.getPrefs().getBoolean("ghostModeStory", false);
                 isGhost_Live_Enabled = XposedPreferences.getPrefs().getBoolean("ghostModeLive", false);
             }
@@ -110,9 +126,11 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
             // Misc options
             is_Misc_Enabled = XposedPreferences.getPrefs().getBoolean("miscOptions", false);
 
-            if (is_Misc_Enabled){
+            if (is_Misc_Enabled) {
                 isStop_Story_Flipping_Enabled = XposedPreferences.getPrefs().getBoolean("storyFlipping", false);
                 isStop_Video_AutoPlay_Enabled = XposedPreferences.getPrefs().getBoolean("videoAutoPlay", false);
+                isShow_Follower_Toast_Enabled = XposedPreferences.getPrefs().getBoolean("followerToast", false);
+                isShow_Enabled_Features_Enabled = XposedPreferences.getPrefs().getBoolean("enabledHookedToast", false);
             }
 
 
@@ -138,6 +156,7 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 
                     // Initialize DexKitBridge with your module's APK (for module-specific tasks, if needed)
                     dexKitBridge = DexKitBridge.create(moduleSourceDir);
+
                     // XposedBridge.log("DexKitBridge initialized for InstaEclipse.");
                 }
 
@@ -167,6 +186,7 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 
                 // Call the method to hook Instagram
                 hookInstagram(lpparam);
+                ;
 
             } catch (Exception e) {
                 XposedBridge.log("(InstaEclipse): Failed to initialize DexKitBridge for Instagram: " + e.getMessage());
@@ -187,33 +207,80 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 
         try {
             uriConditions.clear();
+
             XposedBridge.log("(InstaEclipse): Instagram package detected. Hooking...");
 
             Interceptor interceptor = new Interceptor();
 
+            Testing testing = new Testing();
+            testing.disableSponsoredContent(dexKitBridge, hostClassLoader);
+
             if (isDevEnabled) {
+                FeatureStatusTracker.setEnabled("DevOptions");
+
                 DevOptionsEnable devOptionsEnable = new DevOptionsEnable();
                 devOptionsEnable.handleDevOptions(dexKitBridge);
             }
 
             if (isGhost_Enabled) {
-                if (isGhost_DM_Enabled) {
-                    GhostModeDM ghostModeDM = new GhostModeDM();
-                    ghostModeDM.handleGhostMode(lpparam);
+                if (isGhost_Seen_Enabled) {
+                    FeatureStatusTracker.setEnabled("GhostSeen");
+
+                    SeenState seenState = new SeenState();
+                    seenState.handleSeenBlock(dexKitBridge);
+                }
+
+                if (isGhost_Typing_Enabled) {
+                    FeatureStatusTracker.setEnabled("GhostTyping");
+
+                    TypingStatus typingStatus = new TypingStatus();
+                    typingStatus.handleTypingBlock(dexKitBridge);
+                }
+
+                if (isGhost_Screenshot_Enabled) {
+                    FeatureStatusTracker.setEnabled("GhostScreenshot");
+
+                    ScreenshotDetection screenshotDetection = new ScreenshotDetection();
+                    screenshotDetection.handleScreenshotBlock(dexKitBridge);
+                }
+
+                if (isGhost_View_Once_Enabled) {
+                    FeatureStatusTracker.setEnabled("GhostViewOnce");
+
+                    ViewOnce viewOnce = new ViewOnce();
+                    viewOnce.handleViewOnceBlock(dexKitBridge);
                 }
 
                 if (isGhost_Story_Enabled) {
+                    FeatureStatusTracker.setEnabled("GhostStories");
+
+                    try {
+                        StorySeen storySeen = new StorySeen();
+                        storySeen.handleStorySeenBlock(dexKitBridge);
+                    } catch (Exception ignored) {
+                    }
+
                     uriConditions.add(uri -> uri.getPath().contains("/api/v2/media/seen/"));
                 }
 
                 if (isGhost_Live_Enabled) {
+                    FeatureStatusTracker.setEnabled("GhostLive");
+
+                    try {
+                        LiveSeen liveSeen = new LiveSeen();
+                        liveSeen.handleLiveSeenBlock(dexKitBridge);
+                    } catch (Exception ignored) {
+                    }
+
                     uriConditions.add(uri -> uri.getPath().contains("/heartbeat_and_get_viewer_count/"));
                 }
             }
 
+
             if (isDistraction_Free_Enabled) {
+
                 if (isDistraction_Stories_Enabled) {
-                    uriConditions.add(uri -> uri.getPath().contains("/feed/reels_tray/") || uri.getPath().contains("/api/v1/feed/reels_media_stream/"));
+                    uriConditions.add(uri -> uri.getPath().contains("/feed/reels_tray/"));
                     uriConditions.add(uri -> uri.getPath().contains("feed/get_latest_reel_media/"));
                     uriConditions.add(uri -> uri.getPath().contains("direct_v2/pending_inbox/?visual_message"));
                     uriConditions.add(uri -> uri.getPath().contains("stories/hallpass/"));
@@ -223,6 +290,8 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 
                 }
                 if (isDistraction_Reels_Enabled) {
+
+                    uriConditions.add(uri -> uri.getPath().contains("/api/v1/feed/reels_media_stream/"));
                     uriConditions.add(uri -> ((uri.getPath().endsWith("/qp/batch_fetch/") || uri.getPath().contains("api/v1/clips") || uri.getPath().contains("clips") || uri.getPath().contains("mixed_media") || uri.getPath().contains("mixed_media/discover/stream/"))));
                 }
                 if (isDistraction_Explore_Enabled) {
@@ -235,6 +304,13 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
             }
 
             if (isRemove_Ads_Enabled) {
+                FeatureStatusTracker.setEnabled("AdBlocker");
+                try {
+                    AdBlocker adBlocker = new AdBlocker();
+                    adBlocker.handleAdInsertionBlock(dexKitBridge);
+                } catch (Exception ignored) {
+
+                }
                 uriConditions.add(uri -> uri.getPath().contains("profile_ads/get_profile_ads/"));
                 uriConditions.add(uri -> uri.getPath().contains("/async_ads/"));
                 uriConditions.add(uri -> uri.getPath().contains("/feed/injected_reels_media/"));
@@ -242,19 +318,27 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
             }
 
             if (isRemove_Analytics_Enabled) {
+
                 uriConditions.add(uri -> uri.getHost().contains("graph.instagram.com"));
                 uriConditions.add(uri -> uri.getHost().contains("graph.facebook.com"));
                 uriConditions.add(uri -> uri.getPath().contains("/logging_client_events"));
             }
 
-            if (is_Misc_Enabled){
-                if (isStop_Story_Flipping_Enabled){
+            if (is_Misc_Enabled) {
+                if (isStop_Story_Flipping_Enabled) {
                     StoryFlipping storyFlipping = new StoryFlipping();
                     storyFlipping.handleStoryFlippingDisable(dexKitBridge);
                 }
-                if (isStop_Video_AutoPlay_Enabled){
+                if (isStop_Video_AutoPlay_Enabled) {
                     AutoPlayDisable autoPlayDisable = new AutoPlayDisable();
                     autoPlayDisable.handleAutoPlayDisable(dexKitBridge);
+                }
+                isShow_Follower_Status_Enabled = isShow_Follower_Toast_Enabled;
+
+                if (isShow_Enabled_Features_Enabled) {
+                    // Show toast for enabled/hooked features
+                    CustomToast.hookMainActivity(lpparam);
+
                 }
             }
 

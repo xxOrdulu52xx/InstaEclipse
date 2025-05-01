c5b4e34c3104b5c09a02c4bf9d83e0ac6ad81a82
silvzr <117378129+silvzr@users.noreply.github.com>
2025-05-01 22:41:17 +0200

    Fix and refine TopBar

--- app/src/main/java/ps/reso/instaeclipse/MainActivity.java
+++ app/src/main/java/ps/reso/instaeclipse/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -48,10 +50,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        // Make content appear under the status bar
-        Window window = getWindow();
-        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        window.setStatusBarColor(getResources().getColor(android.R.color.transparent));
+	Toolbar toolbar = findViewById(R.id.top_app_bar);
+	setSupportActionBar(toolbar);
+	ActionBar actionBar = getSupportActionBar();
+	actionBar.setDisplayShowTitleEnabled(false);
 
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
 

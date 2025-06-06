78b6e20c01476a0b7b09c87384333681f15ed577
Abdul <abdulhaleemtamimi@gmail.com>
2025-06-06 14:51:31 +0300

    Remove unwanted key

--- app/src/main/java/ps/reso/instaeclipse/utils/tracker/FollowIndicatorTracker.java
+++ app/src/main/java/ps/reso/instaeclipse/utils/tracker/FollowIndicatorTracker.java
@@ -2,7 +2,6 @@ package ps.reso.instaeclipse.utils.tracker;
 
 public class FollowIndicatorTracker {
     public static String currentlyViewedUserId = null;
-    public static Boolean followsMe = null;
 
 
 }

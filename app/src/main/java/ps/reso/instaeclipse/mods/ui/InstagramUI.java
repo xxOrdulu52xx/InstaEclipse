2f416e506f8695b5a5409890b3e9b9d17a0635e8
Abdul <abdulhaleemtamimi@gmail.com>
2025-06-06 14:51:00 +0300

    Views IDs logger

--- app/src/main/java/ps/reso/instaeclipse/mods/ui/InstagramUI.java
+++ app/src/main/java/ps/reso/instaeclipse/mods/ui/InstagramUI.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+
+import android.content.res.Resources;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.VibrationEffect;
@@ -263,6 +265,26 @@ public class InstagramUI {
             }
         } catch (Exception e) {
             XposedBridge.log("InstaEclipse (vibrate error): " + e.getMessage());
+        }
+    }
+
+    // LOGGER FOR DEV PURPOSES
+    private static void logAllViewIds(View view, Resources res, String packageName, String indent) {
+        if (view.getId() != View.NO_ID) {
+            try {
+                String idName = res.getResourceEntryName(view.getId());
+                XposedBridge.log(indent + "View ID: " + idName + " (" + view.getClass().getSimpleName() + ")");
+            } catch (Resources.NotFoundException e) {
+                // Might be a generated ID or from another package
+                XposedBridge.log(indent + "Unknown ID: " + view.getId() + " (" + view.getClass().getSimpleName() + ")");
+            }
+        }
+
+        if (view instanceof ViewGroup) {
+            ViewGroup group = (ViewGroup) view;
+            for (int i = 0; i < group.getChildCount(); i++) {
+                logAllViewIds(group.getChildAt(i), res, packageName, indent + "  ");
+            }
         }
     }
 

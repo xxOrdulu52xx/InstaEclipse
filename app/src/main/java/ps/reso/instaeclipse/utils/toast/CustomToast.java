3a4f4a271044389bb4b0c9ec1d0939314fea599f
Abdul <abdalhaleem.altamimi@outlook.com>
2025-10-16 15:58:12 +0300

    Remove non-wanted method

--- app/src/main/java/ps/reso/instaeclipse/utils/toast/CustomToast.java
+++ app/src/main/java/ps/reso/instaeclipse/utils/toast/CustomToast.java
@@ -1,22 +1,14 @@
 package ps.reso.instaeclipse.utils.toast;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.Gravity;
 import android.widget.TextView;
 
-import java.util.Map;
-
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import ps.reso.instaeclipse.utils.feature.FeatureStatusTracker;
 
 public class CustomToast {
 
@@ -51,35 +43,4 @@ public class CustomToast {
         });
     }
 
-    public static void hookMainActivity(XC_LoadPackage.LoadPackageParam lpparam) {
-        try {
-            XposedBridge.log("Starting... [1]");
-            XposedHelpers.findAndHookMethod("com.instagram.mainactivity.LauncherActivity", lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) {
-                    XposedBridge.log("Step... [2]");
-                    if (toastShown) return;
-                    toastShown = true;
-
-                    Context context = ((Activity) param.thisObject).getApplicationContext();
-                    if (context == null || !FeatureStatusTracker.hasEnabledFeatures()) return;
-
-                    new Handler(Looper.getMainLooper()).postDelayed(() -> {
-                        XposedBridge.log("Step... [3]");
-                        StringBuilder sb = new StringBuilder("InstaEclipse Loaded 🎯\n");
-
-                        for (Map.Entry<String, Boolean> entry : FeatureStatusTracker.getStatus().entrySet()) {
-                            sb.append(entry.getValue() ? "✅ " : "❌ ").append(entry.getKey()).append("\n");
-                        }
-                        XposedBridge.log("Step... [4]");
-
-                        showCustomToast(context, sb.toString().trim());
-                        XposedBridge.log("Step... [5]");
-                    }, 1000);
-                }
-            });
-        } catch (Throwable t) {
-            XposedBridge.log("❌ Failed to hook LauncherActivity for toast: " + Log.getStackTraceString(t));
-        }
-    }
 }
